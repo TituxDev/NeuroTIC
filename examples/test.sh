a47c19ee9160add087f35fae3ac8de0c2cb6da3f
TituxDev <ocrstmy@gmail.com>
2025-08-09 12:28:17 -0600

    CREATED sctipt for examples ejecution

---
+++ examples/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+example="$1"
+if [ -z "$example" ]; then
+    echo -e "Aviable examples:\033[36m"
+    ls *.c
+    echo -n -e "\033[0m"
+    read -p "Select an example (withour .c extension): " example
+fi
+
+cd ..
+make compile PROJECT_LOCATION=examples PROJECT_NAME=$example PLATAFORM=CPU
+./examples/"$example"
+rm examples/"$example"
